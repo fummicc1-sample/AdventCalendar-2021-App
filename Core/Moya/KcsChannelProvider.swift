5322978aac850bbea7ec2703c96854309db05aa2
fummicc1 <fumiya.tennis1@gmail.com>
2022-01-31 00:37:19 +0900

    :bug: Hide MoyaPovider interface

--- Core/Moya/KcsChannelProvider.swift
+++ Core/Moya/KcsChannelProvider.swift
@@ -1,6 +1,17 @@
 import Foundation
+import CombineMoya
+import Combine
 import Moya
 
-public class KcsChannelProvider: MoyaProvider<KcsChannelService> {
+public class KcsChannelProvider {
 
+    private let provider: MoyaProvider<KcsChannelService> = MoyaProvider()
+
+    public func request(_ targetType: KcsChannelService) -> AnyPublisher<Data, Error> {
+        provider
+            .requestPublisher(targetType, callbackQueue: .global())
+            .map { $0.data }
+            .mapError { $0 as Error }
+            .eraseToAnyPublisher()
+    }
 }
