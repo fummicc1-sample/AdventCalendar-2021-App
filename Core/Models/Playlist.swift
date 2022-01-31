31b69e9ffdb4fe09ea1814dbf9e4d38a565e7929
fummicc1 <fumiya.tennis1@gmail.com>
2022-01-31 21:48:13 +0900

    Fix

--- Core/Models/Playlist.swift
+++ Core/Models/Playlist.swift
@@ -1,14 +1,18 @@
 import Foundation
 
-public struct PlaylistId: ExpressibleByStringLiteral {
-    public let rawValue: String
+public struct PlaylistId: ExpressibleByStringLiteral, Hashable {
+    public let value: String
 
     public init(stringLiteral value: String) {
-        self.rawValue = value
+        self.value = value
+    }
+
+    public init(_ value: String) {
+        self.value = value
     }
 }
 
-public struct Playlist {
+public struct Playlist: Identifiable {
     public var id: PlaylistId
     public var videos: [VideoId]
 
--- Core/Models/Video.swift
+++ Core/Models/Video.swift
@@ -1,14 +1,18 @@
 import Foundation
 
-public struct VideoId: ExpressibleByStringLiteral {
-    public let rawValue: String
+public struct VideoId: ExpressibleByStringLiteral, Hashable {
+    public let value: String
 
     public init(stringLiteral value: String) {
-        self.rawValue = value
+        self.value = value
+    }
+
+    public init(_ value: String) {
+        self.value = value
     }
 }
 
-public struct Video {
+public struct Video: Identifiable {
     public let id: VideoId
 
     public init(id: VideoId) {
