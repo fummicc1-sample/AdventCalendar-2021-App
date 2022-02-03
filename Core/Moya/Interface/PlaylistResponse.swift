ce1686498ddd55d4a2411fb43c8e5326490ed956
fummicc1 <fumiya.tennis1@gmail.com>
2022-02-03 22:07:28 +0900

    Add PlaylistResponse

---
+++ Core/Moya/Interface/PlaylistResponse.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public struct PlaylistResponse: Codable {
+    public let items: [Item]
+}
+
+public extension PlaylistResponse {
+    struct Item: Codable {
+        public let id: String
+    }
+
+    struct Snippet: Codable {
+        public let channelId: String
+        public let title: String
+        public let description: String
+        public let thumbnails: Thumbnails
+    }
+
+    struct Thumbnails: Codable {
+        public let `default`: Thumbnail
+        public let medium: Thumbnail
+        public let high: Thumbnail
+    }
+
+    struct Thumbnail: Codable {
+        public let url: String
+        public let width: Int
+        public let height: Int
+    }
+}
